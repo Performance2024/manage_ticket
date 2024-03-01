65f24ab88f74447bb18208ee9377b6e2eaf488c2
bigdanhe <bigdanhe@gmail.com>
2024-03-02 00:28:23 +0100

    ticket TicketExpress initialisation

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.guest')
+@section('guest_content')
+    <h1 class="text-indigo-500">
+        Guest
+    </h1>
+@endsection
---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('app')
+    @yield('dashboard_content')
+@endsection
